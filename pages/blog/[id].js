cf1218af19a5fd54ff426bf94137a5f3a841b14b
Hitomi Matsuda <matsuda.h@tam-tam.co.jp>
2020-10-01 16:49:59 +0900

    first commit

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,5 +1,5 @@
+// pages/blog/[id].js
 import styles from '../../styles/Home.module.scss'
-
 export default function BlogId({ blog }) {
   return (
     <main className={styles.main}>
@@ -15,66 +15,36 @@ export default function BlogId({ blog }) {
   );
 }
 
-// pages/blog/[id].js
-// export default function BlogId({ blog }) {
-//   return (
-//     <main>
-//       <h1>{blog.title}</h1>
-//       <p>{blog.publishedAt}</p>
-//       <div
-//         dangerouslySetInnerHTML={{
-//           __html: `${blog.body}`,
-//         }}
-//       />
-//     </main>
-//   );
-// }
-// import styles from '../../styles/Home.module.scss'
+export const getStaticPaths = async () => {
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  };
 
-// export default function BlogId({blog}) {
-//   return (
-//     <main className={styles.main}>
-//       <h1 className={styles.title}>{blog.title}</h1>
-//       <p className={styles.publishedAt}>{blog.publishedAt}</p>
-//       <div
-//         dangerouslySetInnerHTML={{
-//           __html: `${blog.body}`,
-//         }}
-//         className={styles.post}
-//       />
-//     </main>
-//   );
-// }
-// export const getStaticPaths = async () => {
-//   const key = {
-//     headers: { 'X-API-KEY': process.env.API_KEY },
-//   };
+  const repos = await fetch('https://my-nextjs.microcms.io/api/v1/blog', key)
+    .then(res => res.json())
+    .catch(() => null);
 
-//   const repos = await fetch('https://my-nextjs.microcms.io/api/v1/blog', key)
-//     .then(res => res.json())
-//     .catch(() => null);
+  const paths = repos.contents.map(repo => `/blog/${repo.id}`);
+  return { paths, fallback: false };
+};
 
-//   const paths = repos.contents.map(repo => `/blog/${repo.id}`);
-//   return { paths, fallback: false };
-// };
+export const getStaticProps = async context => {
+  const id = context.params.id;
 
-// export const getStaticProps = async context => {
-//   const id = context.params.id;
+  const key = {
+    headers: { 'X-API-KEY': process.env.API_KEY },
+  };
 
-//   const key = {
-//     headers: { 'X-API-KEY': process.env.API_KEY },
-//   };
+  const data = await fetch(
+    'https://my-nextjs.microcms.io/api/v1/blog/' + id,
+    key,
+  )
+    .then(res => res.json())
+    .catch(() => null);
 
-//   const data = await fetch(
-//     'https://my-nextjs.microcms.io/api/v1/blog/' + id,
-//     key,
-//   )
-//     .then(res => res.json())
-//     .catch(() => null);
-
-//   return {
-//     props: {
-//       blog: data,
-//     },
-//   };
-// };
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
